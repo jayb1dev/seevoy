7c7f67ba6d8f5e9d9c6c2088d6c74f85e8c68971
jmac <jason.mac.laptop@gmail.com>
2025-04-13 23:36:46 -0700

    Fix tab bar

--- src/routes/pages/settings/about/AppDetails.tsx
+++ src/routes/pages/settings/about/AppDetails.tsx
@@ -20,7 +20,7 @@ export default function AppDetails() {
       </div>
       
       <div className={styles.padding}>
-            VERSION 1.0.319
+            VERSION 1.0.320
       </div>
 
       <div className={styles.padding}>
