7a8c72ed6f29ba7d7e1411bda9deb072ed467aea
jmac <jason.mac.laptop@gmail.com>
2025-04-14 22:25:38 -0700

    Update build.

--- src/routes/pages/settings/about/AppDetails.tsx
+++ src/routes/pages/settings/about/AppDetails.tsx
@@ -20,7 +20,7 @@ export default function AppDetails() {
       </div>
       
       <div className={styles.padding}>
-            VERSION 1.0.321
+            VERSION 1.0.322
       </div>
 
       <div className={styles.padding}>
