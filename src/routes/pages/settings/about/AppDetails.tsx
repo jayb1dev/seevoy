30110da82983c68577af436ce18a491adc71026a
jmac <jason.mac.laptop@gmail.com>
2025-05-06 19:14:46 -0700

    Add img link for failed media load.

--- src/routes/pages/settings/about/AppDetails.tsx
+++ src/routes/pages/settings/about/AppDetails.tsx
@@ -20,7 +20,7 @@ export default function AppDetails() {
       </div>
       
       <div className={styles.padding}>
-            VERSION 1.0.385
+            VERSION 1.0.408
       </div>
 
       <div className={styles.padding}>
